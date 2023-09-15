375abb998952c7faa1e6d72269e9e1e19cb726f0
Jack Schofield <59843981+jschof1@users.noreply.github.com>
2023-09-15 17:41:31 +0100

    update build module 4

---
+++ docs/course/en/selectchoice.js
@@ -0,0 +1,571 @@
+
+  {
+    "_id": "aide-m3-o5-third-party-b-2714833",
+    "_parentId": "aide-m3-o5-third-party-b-230",
+    "_type": "component",
+    "_component": "selectchoice",
+    "_classes": "",
+    "_layout": "full",
+    "title": "Select Choice",
+    "displayTitle": "Select Choice",
+    "body": "",
+    "instruction": "",
+    "_attempts": 1,
+    "_shouldDisplayAttempts": false,
+    "_shouldResetAllAnswers": false,
+    "_isRandom": false,
+    "_questionWeight": 1,
+    "_canShowModelAnswer": true,
+    "_canShowFeedback": true,
+    "_canShowMarking": true,
+    "_recordInteraction": true,
+    "_hasItemScoring": true,
+    "_choices": [
+      {
+        "text": "Very important",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        }
+      },
+      {
+        "text": "Secondary consideration",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        }
+      },
+      {
+        "text": "not important to me",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        }
+      }
+    ],
+    "_items": [
+      {
+        "text": "I would consider the potential consequences of confronting my coworker. If exposing the truth could harm the team's dynamics and jeopardise the project, I might choose to address the issue privately and encourage a collaborative approach going forward.",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        },
+        "_shouldBeSelected": 1
+      }
+    ],
+    "_feedback": {
+      "title": "",
+      "correct": "<p>A utilitarian approach might prioritise the overall well-being of the team and the project's success. The focus is on achieving the best outcome for everyone involved, even if it means avoiding direct confrontation.</p>",
+      "_incorrect": {
+        "notFinal": "",
+        "final": "<p>A utilitarian approach might prioritise the overall well-being of the team and the project's success. The focus is on achieving the best outcome for everyone involved, even if it means avoiding direct confrontation.</p>"
+      },
+      "_partlyCorrect": {
+        "notFinal": "",
+        "final": "<p>A utilitarian approach might prioritise the overall well-being of the team and the project's success. The focus is on achieving the best outcome for everyone involved, even if it means avoiding direct confrontation.</p>"
+      }
+    },
+    "_buttons": {
+      "_submit": {
+        "buttonText": "Submit",
+        "ariaLabel": "Select here to submit your answer."
+      },
+      "_reset": {
+        "buttonText": "Reset",
+        "ariaLabel": ""
+      },
+      "_showCorrectAnswer": {
+        "buttonText": "Correct Answer",
+        "ariaLabel": ""
+      },
+      "_hideCorrectAnswer": {
+        "buttonText": "My Answer",
+        "ariaLabel": ""
+      },
+      "_showFeedback": {
+        "buttonText": "Show feedback",
+        "ariaLabel": ""
+      },
+      "remainingAttemptsText": "attempts remaining",
+      "remainingAttemptText": "final attempt"
+    }
+  },
+  {
+    "_id": "aide-m3-o5-third-party-b-2110490",
+    "_parentId": "aide-m3-o5-third-party-b-231",
+    "_type": "component",
+    "_component": "selectchoice",
+    "_classes": "",
+    "_layout": "full",
+    "title": "Select Choice",
+    "displayTitle": "Select Choice",
+    "body": "",
+    "instruction": "",
+    "_attempts": 1,
+    "_shouldDisplayAttempts": false,
+    "_shouldResetAllAnswers": false,
+    "_isRandom": false,
+    "_questionWeight": 1,
+    "_canShowModelAnswer": true,
+    "_canShowFeedback": true,
+    "_canShowMarking": true,
+    "_recordInteraction": true,
+    "_hasItemScoring": true,
+    "_choices": [
+      {
+        "text": "Very important",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        }
+      },
+      {
+        "text": "Secondary consideration",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        }
+      },
+      {
+        "text": "not important to me",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        }
+      }
+    ],
+    "_items": [
+      {
+        "text": "I would feel compelled to uphold the principle of honesty and fairness. I would confront my coworker privately and share my concerns with them, emphasising the importance of acknowledging the team's contributions accurately.",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        },
+        "_shouldBeSelected": 1
+      }
+    ],
+    "_feedback": {
+      "title": "",
+      "correct": "<p>A deontological approach places a strong emphasis on moral duties and principles, such as truthfulness and fairness, regardless of the potential consequences. Upholding the ethical rule of honesty is the primary driver of the decision.</p>",
+      "_incorrect": {
+        "notFinal": "",
+        "final": "<p>A deontological approach places a strong emphasis on moral duties and principles, such as truthfulness and fairness, regardless of the potential consequences. Upholding the ethical rule of honesty is the primary driver of the decision.</p>"
+      },
+      "_partlyCorrect": {
+        "notFinal": "",
+        "final": "<p>A deontological approach places a strong emphasis on moral duties and principles, such as truthfulness and fairness, regardless of the potential consequences. Upholding the ethical rule of honesty is the primary driver of the decision.</p>"
+      }
+    },
+    "_buttons": {
+      "_submit": {
+        "buttonText": "Submit",
+        "ariaLabel": "Select here to submit your answer."
+      },
+      "_reset": {
+        "buttonText": "Reset",
+        "ariaLabel": ""
+      },
+      "_showCorrectAnswer": {
+        "buttonText": "Correct Answer",
+        "ariaLabel": ""
+      },
+      "_hideCorrectAnswer": {
+        "buttonText": "My Answer",
+        "ariaLabel": ""
+      },
+      "_showFeedback": {
+        "buttonText": "Show feedback",
+        "ariaLabel": ""
+      },
+      "remainingAttemptsText": "attempts remaining",
+      "remainingAttemptText": "final attempt"
+    }
+  },
+  {
+    "_id": "aide-m3-o5-third-party-b-2402622",
+    "_parentId": "aide-m3-o5-third-party-b-232",
+    "_type": "component",
+    "_component": "selectchoice",
+    "_classes": "",
+    "_layout": "full",
+    "title": "Select Choice",
+    "displayTitle": "Select Choice",
+    "body": "",
+    "instruction": "",
+    "_attempts": 1,
+    "_shouldDisplayAttempts": false,
+    "_shouldResetAllAnswers": false,
+    "_isRandom": false,
+    "_questionWeight": 1,
+    "_canShowModelAnswer": true,
+    "_canShowFeedback": true,
+    "_canShowMarking": true,
+    "_recordInteraction": true,
+    "_hasItemScoring": true,
+    "_choices": [
+      {
+        "text": "Very important",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        }
+      },
+      {
+        "text": "Secondary consideration",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        }
+      },
+      {
+        "text": "not important to me",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        }
+      }
+    ],
+    "_items": [
+      {
+        "text": "My instinct would be to address the issue openly and assertively while maintaining a calm and respectful demeanour. I believe in fostering a workplace environment built on integrity and open communication.",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        },
+        "_shouldBeSelected": 1
+      }
+    ],
+    "_feedback": {
+      "title": "",
+      "correct": "<p>A virtue ethics perspective focuses on developing positive character traits and virtues. The emphasis is on being honest, assertive, and fostering integrity in the workplace.</p>",
+      "_incorrect": {
+        "notFinal": "",
+        "final": "<p>A virtue ethics perspective focuses on developing positive character traits and virtues. The emphasis is on being honest, assertive, and fostering integrity in the workplace.</p>"
+      },
+      "_partlyCorrect": {
+        "notFinal": "",
+        "final": "<p>A virtue ethics perspective focuses on developing positive character traits and virtues. The emphasis is on being honest, assertive, and fostering integrity in the workplace.</p>"
+      }
+    },
+    "_buttons": {
+      "_submit": {
+        "buttonText": "Submit",
+        "ariaLabel": "Select here to submit your answer."
+      },
+      "_reset": {
+        "buttonText": "Reset",
+        "ariaLabel": ""
+      },
+      "_showCorrectAnswer": {
+        "buttonText": "Correct Answer",
+        "ariaLabel": ""
+      },
+      "_hideCorrectAnswer": {
+        "buttonText": "My Answer",
+        "ariaLabel": ""
+      },
+      "_showFeedback": {
+        "buttonText": "Show feedback",
+        "ariaLabel": ""
+      },
+      "remainingAttemptsText": "attempts remaining",
+      "remainingAttemptText": "final attempt"
+    }
+  },
+  {
+    "_id": "aide-m3-o5-third-party-b-2856139",
+    "_parentId": "aide-m3-o5-third-party-b-233",
+    "_type": "component",
+    "_component": "selectchoice",
+    "_classes": "",
+    "_layout": "full",
+    "title": "Select Choice",
+    "displayTitle": "Select Choice",
+    "body": "",
+    "instruction": "",
+    "_attempts": 1,
+    "_shouldDisplayAttempts": false,
+    "_shouldResetAllAnswers": false,
+    "_isRandom": false,
+    "_questionWeight": 1,
+    "_canShowModelAnswer": true,
+    "_canShowFeedback": true,
+    "_canShowMarking": true,
+    "_recordInteraction": true,
+    "_hasItemScoring": true,
+    "_choices": [
+      {
+        "text": "Very important",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        }
+      },
+      {
+        "text": "Secondary consideration",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        }
+      },
+      {
+        "text": "not important to me",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        }
+      }
+    ],
+    "_items": [
+      {
+        "text": "I would discuss the situation with my coworker and supervisor to ensure that credit is fairly distributed among the team members.",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        },
+        "_shouldBeSelected": 1
+      }
+    ],
+    "_feedback": {
+      "title": "",
+      "correct": "<p>A <strong>justice and fairness</strong> focuses on fairness, addressing biases, and striving for equitable outcomes.</p>",
+      "_incorrect": {
+        "notFinal": "",
+        "final": "<p>A <strong>justice and fairness</strong> focuses on fairness, addressing biases, and striving for equitable outcomes.</p>"
+      },
+      "_partlyCorrect": {
+        "notFinal": "",
+        "final": "<p>A <strong>justice and fairness</strong> perspective focuses on fairness, addressing biases, and striving for equitable outcomes.</p>"
+      }
+    },
+    "_buttons": {
+      "_submit": {
+        "buttonText": "Submit",
+        "ariaLabel": "Select here to submit your answer."
+      },
+      "_reset": {
+        "buttonText": "Reset",
+        "ariaLabel": ""
+      },
+      "_showCorrectAnswer": {
+        "buttonText": "Correct Answer",
+        "ariaLabel": ""
+      },
+      "_hideCorrectAnswer": {
+        "buttonText": "My Answer",
+        "ariaLabel": ""
+      },
+      "_showFeedback": {
+        "buttonText": "Show feedback",
+        "ariaLabel": ""
+      },
+      "remainingAttemptsText": "attempts remaining",
+      "remainingAttemptText": "final attempt"
+    }
+  },
+  {
+    "_id": "aide-m3-o5-third-party-b-2998365",
+    "_parentId": "aide-m3-o5-third-party-b-234",
+    "_type": "component",
+    "_component": "selectchoice",
+    "_classes": "",
+    "_layout": "full",
+    "title": "Select Choice",
+    "displayTitle": "Select Choice",
+    "body": "",
+    "instruction": "",
+    "_attempts": 1,
+    "_shouldDisplayAttempts": false,
+    "_shouldResetAllAnswers": false,
+    "_isRandom": false,
+    "_questionWeight": 1,
+    "_canShowModelAnswer": true,
+    "_canShowFeedback": true,
+    "_canShowMarking": true,
+    "_recordInteraction": true,
+    "_hasItemScoring": true,
+    "_choices": [
+      {
+        "text": "Very important",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        }
+      },
+      {
+        "text": "Secondary consideration",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        }
+      },
+      {
+        "text": "not important to me",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        }
+      }
+    ],
+    "_items": [
+      {
+        "text": "I would recognise that my coworker's actions undermine the rights of the team members who deserve credit for their work. I would stand up for their rights and advocate for proper recognition.",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        },
+        "_shouldBeSelected": 1
+      }
+    ],
+    "_feedback": {
+      "title": "",
+      "correct": "<p>A rights ethics approach centres on upholding individual rights and ensuring fairness. The focus is on advocating for the rights of team members who deserve proper acknowledgment.</p>",
+      "_incorrect": {
+        "notFinal": "",
+        "final": "<p>A rights ethics approach centres on upholding individual rights and ensuring fairness. The focus is on advocating for the rights of team members who deserve proper acknowledgment.</p>"
+      },
+      "_partlyCorrect": {
+        "notFinal": "",
+        "final": "<p>A rights ethics approach centres on upholding individual rights and ensuring fairness. The focus is on advocating for the rights of team members who deserve proper acknowledgment.</p>"
+      }
+    },
+    "_buttons": {
+      "_submit": {
+        "buttonText": "Submit",
+        "ariaLabel": "Select here to submit your answer."
+      },
+      "_reset": {
+        "buttonText": "Reset",
+        "ariaLabel": ""
+      },
+      "_showCorrectAnswer": {
+        "buttonText": "Correct Answer",
+        "ariaLabel": ""
+      },
+      "_hideCorrectAnswer": {
+        "buttonText": "My Answer",
+        "ariaLabel": ""
+      },
+      "_showFeedback": {
+        "buttonText": "Show feedback",
+        "ariaLabel": ""
+      },
+      "remainingAttemptsText": "attempts remaining",
+      "remainingAttemptText": "final attempt"
+    }
+  },
+  {
+    "_id": "aide-m3-o5-third-party-b-2224256",
+    "_parentId": "aide-m3-o5-third-party-b-235",
+    "_type": "component",
+    "_component": "selectchoice",
+    "_classes": "",
+    "_layout": "full",
+    "title": "Select Choice",
+    "displayTitle": "Select Choice",
+    "body": "",
+    "instruction": "",
+    "_attempts": 1,
+    "_shouldDisplayAttempts": false,
+    "_shouldResetAllAnswers": false,
+    "_isRandom": false,
+    "_questionWeight": 1,
+    "_canShowModelAnswer": true,
+    "_canShowFeedback": true,
+    "_canShowMarking": true,
+    "_recordInteraction": true,
+    "_hasItemScoring": true,
+    "_choices": [
+      {
+        "text": "Very important",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        }
+      },
+      {
+        "text": "Secondary consideration",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        }
+      },
+      {
+        "text": "not important to me",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        }
+      }
+    ],
+    "_items": [
+      {
+        "text": "I would approach my coworker with empathy, aiming to understand their perspective while also expressing how the situation has affected the team's morale. Prioritising open dialogue and relationships guides my response.",
+        "_graphic": {
+          "alt": "",
+          "large": "",
+          "small": ""
+        },
+        "_shouldBeSelected": 1
+      }
+    ],
+    "_feedback": {
+      "title": "",
+      "correct": "An ethics of care perspective focuses on empathy, maintaining relationships, and considering the impact on team dynamics.</p>",
+      "_incorrect": {
+        "notFinal": "",
+        "final": "An ethics of care perspective focuses on empathy, maintaining relationships, and considering the impact on team dynamics.</p>"
+      },
+      "_partlyCorrect": {
+        "notFinal": "",
+        "final": "An ethics of care perspective focuses on empathy, maintaining relationships, and considering the impact on team dynamics.</p>"
+      }
+    },
+    "_buttons": {
+      "_submit": {
+        "buttonText": "Submit",
+        "ariaLabel": "Select here to submit your answer."
+      },
+      "_reset": {
+        "buttonText": "Reset",
+        "ariaLabel": ""
+      },
+      "_showCorrectAnswer": {
+        "buttonText": "Correct Answer",
+        "ariaLabel": ""
+      },
+      "_hideCorrectAnswer": {
+        "buttonText": "My Answer",
+        "ariaLabel": ""
+      },
+      "_showFeedback": {
+        "buttonText": "Show feedback",
+        "ariaLabel": ""
+      },
+      "remainingAttemptsText": "attempts remaining",
+      "remainingAttemptText": "final attempt"
+    }
+  },
